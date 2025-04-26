bf439f0c4ca636eedd7c87d656550012d7144459
Sergey Boykov <sutemi67@gmail.com>
2025-04-26 21:37:29 +0300

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by SERGEY BOYKOV on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestProjectTests/TestProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestProjectTests.swift
+//  TestProjectTests
+//
+//  Created by SERGEY BOYKOV on 26.04.2025.
+//
+
+import Testing
+
+struct TestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
